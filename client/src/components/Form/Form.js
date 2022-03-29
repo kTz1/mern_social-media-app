1c3df409f743a26b1c8099fa9855d1988e198fd6
kTz1 <catalin_simon90@yahoo.com>
2022-03-29 18:52:55 +0300

    added a api request and the action for the post request

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import  { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64';
+import { useDispatch } from 'react-redux';
 
+import { createPost } from '../../actions/posts';
 import useStyles from './styles';
 
 const Form = () => {
@@ -13,8 +15,13 @@ const Form = () => {
         selectedFile: ''
     });
     const classes = useStyles();
+    const dispatch = useDispatch();
 
-    const handleSubmit = () => {};
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        dispatch(createPost(postData));
+    };
 
     const clear = () => {};
 
--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://kTz:ARZtXvmJZjrLoEOr@cluster0.bw2zv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
