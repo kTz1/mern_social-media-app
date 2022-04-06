e84f3c38f85e50760032396a0ebf15dfff7d50a3
kTz1 <catalin_simon90@yahoo.com>
2022-04-06 16:50:11 +0300

    added Pagination, PostDetails + redirect

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { Container, Grow, Grid } from '@material-ui/core';
+import { Container, Grow, Grid, Paper } from '@material-ui/core';
 
 import { getPosts } from '../../actions/posts';
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
+import Pagination from '../Pagination/Pagination';
 import useStyles from './styles';
 
 const Home = () => {
@@ -25,6 +26,9 @@ const Home = () => {
                     </Grid>
                     <Grid item xs={12} sm={4}>
                         <Form currentId={currentId} setCurrentId={setCurrentId} />
+                        <Paper className={classes.pagination} elevation={6}>
+                            <Pagination />
+                        </Paper>
                     </Grid>
                 </Grid>
             </Container>
